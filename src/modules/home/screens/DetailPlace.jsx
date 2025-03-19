a287b467fbb514f84af793d8754eceb23debbc54
LeobardoVillalobos88 <20233tn098@utez.edu.mx>
2025-03-19 11:48:48 -0600

    Primer commit de proyecto Expo Airbnb

--- src/modules/home/screens/DetailPlace.jsx
+++ src/modules/home/screens/DetailPlace.jsx
@@ -1,42 +1,43 @@
 import { StyleSheet, View, ScrollView, Text } from 'react-native'
-import { Image, AirbnbRating } from '@rneui/base';
-import React, {useLayoutEffect} from 'react'
+import { Image, AirbnbRating } from "@rneui/base";
+import React, { useLayoutEffect } from 'react'
 import PagerView from 'react-native-pager-view';
-import {map} from "lodash";
-
+import { map } from 'lodash';
+import MapViewCustom from '../../../kernel/components/MapViewCustom';
 export default function DetailPlace(props) {
-    //VAMOS A RECIBIR DEL ROUTE EL ELEMENTO PLACE EL CUAL CONTENDRA TODAS SUS PROPIEDADES
-    const {navigation, route} = props;
-    const {place} = route.params;
-    //VAMOS A MODIFICAR LA CABECERA DE MI STACK, COLOCANDOLO COMO TITULO PLACE
-    useLayoutEffect(() => {
-        navigation.setOptions({title: place.name});
-    }, [navigation]);
+  const { place } = props.route.params;
+  useLayoutEffect(() => {
+    props.navigation.setOptions({
+      title: place.name
+    })
+  }, [props.navigation])
   return (
     <View style={styles.container}>
       <ScrollView>
-      <PagerView style={styles.PagerView} initialPage={0}>
-        {map(place.images, (image, index) => (
-          <View style={styles.page} key={index}>
-            <Image source={{uri: image}} style={{width: "100%", height: "100%"}} resizeMode='cover'></Image>
-          </View>
-        ))}
-      </PagerView>
-
-      <View style={{flexDirection: "row", justifyContent: "space-between", marginHorizontal: 16, marginVertical: 8}}>
-
-         <Text style={{ fontWeight: "bold" }}>{name}</Text>
-                    <AirbnbRating 
-                    count={5}
-                    defaultRating={place.rating/place.count}
-                    size={12}
-                    showRating={false}
-                    isDisabled={true} />
-      </View>
-
-      <Text style={{ color: "gray",  margin: 16 }}>{place.description}</Text> 
+        <PagerView style={styles.pagerView} initialPage={0}>
+          {map(place.images, (image, index) => {
+            return (
+              <View style={styles.page} key={index}>
+                <Image source={{ uri: image }} style={{ width: '100%', height: "100%" }} resizeMode='cover' />
+              </View>
+            )
+          })}
+        </PagerView>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 16, marginVertical: 8 }}>
+          <Text style={{ fontWeight: 'bold' }}>{place.name}</Text>
+          <AirbnbRating
+            count={5}
+            defaultRating={place.rating / place.count}
+            size={20}
+            showRating={false}
+            isDisabled={true}
+          />
+        </View>
+        <Text style={{color:"gray", margin: 16}}>{place.description}</Text>
+        <MapViewCustom/>
       </ScrollView>
-      
+
+
     </View>
   )
 }
@@ -44,14 +45,13 @@ export default function DetailPlace(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white'
+    backgroundColor: "white",
   },
-  PagerView:{
-    height: 256
+  pagerView: {
+    height: 256,
   },
   page: {
-    flex: 1,
     width: "100%",
-    height: 256,
-  }
+    height: 256
+  },
 })
